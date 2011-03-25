37e77761a54f6a0a32646a5f298f146ec6841c61
gbostoen <glennbostoen84@gmail.com>
2011-03-25 19:48:26 +0100

    progressdialog tijdens het inloggen

--- src/com/vop/augumented/StartupActivity.java
+++ src/com/vop/augumented/StartupActivity.java
@@ -1,5 +1,7 @@
 package com.vop.augumented;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,28 +16,47 @@ import com.vop.tools.VopApplication;
 import com.vop.tools.data.Person;
 
 public class StartupActivity extends FullscreenActivity {
+	static Context context;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.startupactivity_layout);
+		context = getApplicationContext();
 	}
 
 	// go-klik
 	public void go_klik(View v) {
-		EditText emailbox = (EditText)findViewById(R.id.invul_box);
-		VopApplication app = (VopApplication)getApplicationContext();
+		final EditText emailbox = (EditText) findViewById(R.id.invul_box);
+		final VopApplication app = (VopApplication) getApplicationContext();
 		app.putState("login", "true");
-		
-		Person p = DBWrapper.getProfile(emailbox.getText().toString());
-		
-		if (p != null) {
-			app.putState("userid", p.getId().toString());
-	    	Intent myIntent = new Intent(StartupActivity.this, Hoofdmenu.class);
-	    	StartupActivity.this.startActivity(myIntent);
-		} else {
-			Toast.makeText(getApplicationContext(), "emailaddress not valid", Toast.LENGTH_SHORT).show();
-		}
+		final ProgressDialog dialog = ProgressDialog.show(this, "",
+				"Bezig met inloggen. Even geduld...", true);
+		new Thread() {
+			public void run() {
+				try {
+					Person p = DBWrapper.getProfile(emailbox.getText()
+							.toString());
+					if (p != null) {
+						app.putState("userid", p.getId().toString());
+						Intent myIntent = new Intent(StartupActivity.this,
+								Hoofdmenu.class);
+						StartupActivity.this.startActivity(myIntent);
+						dialog.dismiss();
+					} else {
+						dialog.dismiss();
+						runOnUiThread(new Runnable() {
+							public void run() {
+								Toast.makeText(getApplicationContext(),
+										"emailadres is niet gevonden",
+										Toast.LENGTH_SHORT).show();
+							}
+						});
+					}
+				} catch (Exception e) {
+				}
+			}
+		}.start();
 	}
 
 	// menu openen
