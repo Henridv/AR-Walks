14636d72a1e3d6d99315c769dfbe4cd3ebb4a6eb
Lynn <lynn.verbeke@gmail.com>
2011-04-12 10:09:53 +0200

    toon profiel

--- src/com/vop/augumented/Profiel.java
+++ src/com/vop/augumented/Profiel.java
@@ -3,8 +3,12 @@ package com.vop.augumented;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.TextView;
 
+import com.vop.tools.DBWrapper;
 import com.vop.tools.FullscreenActivity;
+import com.vop.tools.VopApplication;
+import com.vop.tools.data.Person;
 
 public class Profiel extends FullscreenActivity {
 
@@ -12,6 +16,19 @@ public class Profiel extends FullscreenActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.profiel_layout);
+		VopApplication app = (VopApplication) getApplicationContext();
+		int id = Integer.parseInt(app.getState().get("userid"));
+		Person p = DBWrapper.getProfile(id);
+		TextView naam = new TextView(this);
+		naam=(TextView)findViewById(R.id.textView1); 
+	    naam.setText("naam: "+p.getName());
+	    TextView tel = new TextView(this);
+		tel=(TextView)findViewById(R.id.textView2); 
+	    tel.setText("phone number: "+p.getPhone());
+	    TextView email = new TextView(this);
+		email=(TextView)findViewById(R.id.textView3); 
+	    email.setText("e-mail: "+p.getEmail());
+
 
 	}
 
