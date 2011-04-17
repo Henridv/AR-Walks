2d4570b539eaeaef3beff1a022797827fcb03bdb
Lynn <lynn.verbeke@gmail.com>
2011-04-17 17:35:01 +0200

    vrienden_ongetest

--- src/com/vop/augumented/Vrienden.java
+++ src/com/vop/augumented/Vrienden.java
@@ -2,6 +2,10 @@ package com.vop.augumented;
 
 import java.util.ArrayList;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,6 +17,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 
 import com.vop.tools.DBWrapper;
 import com.vop.tools.FullscreenListActivity;
@@ -26,33 +31,47 @@ public class Vrienden extends FullscreenListActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		VopApplication app = (VopApplication) getApplicationContext();
-		int id = Integer.parseInt(app.getState().get("userid"));
-
-		vrienden = DBWrapper.getFriends(id);
-
-		String[] res = new String[vrienden.size()];
-		{
-			for (int i = 0; i < vrienden.size(); i++) {
-				res[i] = vrienden.get(i).getName();
-			}
-		}
-
-		setListAdapter(new ArrayAdapter<String>(this, R.layout.vrienden_layout,
-				res));
-
+		updateFriends();
 		ListView lv = getListView();
 		lv.setTextFilterEnabled(true);
-
+		
+		
 		lv.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 				// When clicked, show a toast with the TextView text
-				Toast.makeText(getApplicationContext(),
-						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+				Toast.makeText(Vrienden.this, "not yet implemented", Toast.LENGTH_SHORT).show();
 			}
 		});
+		
+		lv.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					final int position, long id) {
+				final CharSequence[] items = { "Send Message", "Delete" };
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(Vrienden.this);
+				builder.setTitle(vrienden.get(position).getName());
+				builder.setItems(items, new OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int item) {
+						if (items[item].equals("Send Message")) {
+							Toast.makeText(Vrienden.this, "not yet implemented", Toast.LENGTH_SHORT).show();
+						} else if (items[item].equals("Delete")) {
+							DBWrapper.delete(vrienden.get(position));
+							updateFriends();
+						}
+					}
+				});
+				AlertDialog alert = builder.create();
+				alert.show();
+
+				return true;
+			}
+		});
+
 	}
 
 	// menu openen
@@ -67,13 +86,36 @@ public class Vrienden extends FullscreenListActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle item selection
 		switch (item.getItemId()) {
-		case R.id.refresh:
-			Toast.makeText(getApplicationContext(),
-					"u selecteerde:" + DBWrapper.getFriends(0),
-					Toast.LENGTH_SHORT).show();
+		case R.id.update_friends:
+			updateFriends();
 			return true;
 		default:
+			//add a friend!!
 			return super.onOptionsItemSelected(item);
 		}
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		updateFriends();
+	}
+	
+	private void updateFriends(){
+		VopApplication app = (VopApplication) getApplicationContext();
+		int id = Integer.parseInt(app.getState().get("userid"));
+
+		vrienden = DBWrapper.getFriends(id);
+
+		String[] res = new String[vrienden.size()];
+		{
+			for (int i = 0; i < vrienden.size(); i++) {
+				res[i] = vrienden.get(i).getName();
+			}
+		}
+
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.vrienden_layout,
+				res));
+		
 	}
 }
